7fb022e3345aae960c8a32a5c594f28a66cb790a
jtoralo <jtoralo@tsir-47.alumno.upv.es>
2025-10-18 18:37:33 +0200

    he quitado el login para navegar (de momento no hace falta hacerlo) y quitado más catalanes, falta hacerlo bonito y cambiar los emojis por fotos de los coches

---
+++ src/java/beans/LogIn.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beans;
+
+import javax.inject.Named;
+import javax.enterprise.context.Dependent;
+
+/**
+ *
+ * @author jtoralo
+ */
+@Named(value = "logIn")
+@Dependent
+public class LogIn {
+    private String name, pwd;
+
+    public LogIn() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+    
+}
